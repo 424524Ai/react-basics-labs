b9ae8ac300b06c84dd16a678bd18c73ea86f46e6
hleab <hleabrszylx@gmail.com>
2025-09-23 12:58:43 +0100

    Add task functionality

--- tasky/src/App.jsx
+++ tasky/src/App.jsx
@@ -2,6 +2,7 @@ import './App.css';
 import Task from './components/Task';
 import React, {useState} from 'react';
 import AddTaskForm from './components/Form';
+import { v4 as uuidv4 } from 'uuid';
 
 // 项目的根组件
 // 流程
@@ -17,6 +18,13 @@ function App() {
       {id: 3, title: "Tidy", deadline: "Today", priority: "High", done: false} // tasks数组元素[2]
     ]
   })
+
+  // form object = 对象 存储用户输入的任务信息
+  const [ formState, setFormState ] = useState({
+    title: "",
+    description: "",
+    deadline: ""
+  });
 
   const doneHandler = (taskIndex) => {
     // 1️⃣ 复制当前任务数组（浅拷贝) - 为了重新渲染
@@ -37,6 +45,43 @@ function App() {
     setTaskState({tasks});
   }
 
+  // 用来处理用户填写的任务信息的更新
+  const formChangeHandler = (event) => {
+    let form = {...formState};
+
+    switch(event.target.name) {
+      case "title":
+          form.title = event.target.value;
+          break;
+      case "description":
+          form.description = event.target.value;
+          break;
+      case "deadline":
+          form.deadline = event.target.value;
+          break;
+      default:
+          form = formState;
+    }
+    setFormState(form);
+  }
+
+  console.log(formState);
+
+    const formSubmitHandler = (event) => {
+    event.preventDefault();
+
+    // 浅拷贝tasks 和 form
+    const tasks = [...taskState.tasks];
+    const form = {...formState};
+
+    // 用uuid包给form添加uid
+    form.id = uuidv4();
+    
+    // 将form添加到tasks 数组
+    tasks.push(form);
+    setTaskState({tasks});
+  }
+
   // 渲染给用户看的信息
   return (
     <div className="container">
@@ -52,7 +97,7 @@ function App() {
       deleteTask = {() => deleteHandler(index)}
       /> 
       ))} 
-      <AddTaskForm />
+      <AddTaskForm submit={formSubmitHandler} change={formChangeHandler}/>
     </div>
   );
 }
