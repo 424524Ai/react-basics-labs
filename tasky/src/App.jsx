13ae38f2f6d21d19ccb5311e6e86bad0371b072e
hleab <hleabrszylx@gmail.com>
2025-09-23 12:02:29 +0100

    Tasky Lab 2a - Exercise done

--- tasky/src/App.jsx
+++ tasky/src/App.jsx
@@ -8,9 +8,9 @@ import React, {useState} from 'react';
 function App() {
   const [taskState, setTaskState] = useState({
     tasks: [
-      {id: 1, title: "Dishes", description: "Empty dishwasher", deadline: "Today"},
-      {id: 2, title: "Laundry", description: "Fold laundry and put away", deadline: "Tomorrow"},
-      {id: 3, title: "Tidy", deadline: "Today"}
+      {id: 1, title: "Dishes", description: "Empty dishwasher", deadline: "Today", priority: "Low"},
+      {id: 2, title: "Laundry", description: "Fold laundry and put away", deadline: "Tomorrow", priority: "Medium"},
+      {id: 3, title: "Tidy", deadline: "Today", priority: "High"}
     ]
   })
 
@@ -18,7 +18,7 @@ function App() {
     <div className="container">
       <h1>Tasky</h1>
       {taskState.tasks.map((task) => (              
-      <Task title = {task.title} description = {task.description} deadline = {task.deadline} key = {task.id} /> 
+      <Task title = {task.title} description = {task.description} deadline = {task.deadline} priority = {task.priority} key = {task.id} /> 
       ))} 
     </div>
   );
